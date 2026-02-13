e5b24861d986c19e577fa7c0aa3d66f685bb685d
Reemaalazri <146677257+Reemaalazri@users.noreply.github.com>
2026-02-13 14:32:55 +0000

    Add initial Claim model

--- src/claims/models.py
+++ src/claims/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create models
+class Claim(models.Model):
+    liar_id = models.CharField(max_length=50)
+    label = models.CharField(max_length=20)
+    statement = models.TextField()
+    speaker = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.statement[:50]
