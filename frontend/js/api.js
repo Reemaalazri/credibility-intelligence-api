f52f95151152ee813dd7a980b081c3a6cad302cf
Reemaalazri <146677257+Reemaalazri@users.noreply.github.com>
2026-03-10 20:05:41 +0000

    Point frontend to deployed Render API

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://credibility-intelligence-api.onrender.com";
 
 function getAccessToken() {
   return localStorage.getItem("access");
